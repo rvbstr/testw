6c8dbf73ecd9bd2f5899ddd0ad9d71dcfac8f3fd
rvbstr <nechaevnv@gmail.com>
2023-11-13 15:24:51 +0300

    js added

---
+++ script.js
@@ -0,0 +1,7 @@
+function pageHideHome(){
+    var x = document.getElementById("home");
+    x.style.display = "none";
+    var y = document.getElementById("cur");
+    y.style.display = "flex";
+}
+
