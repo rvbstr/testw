fb95c700290d1c99d9d76e586346609da1d8bc48
rvbstr <nechaevnv@gmail.com>
2023-11-13 19:42:25 +0300

    js update

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function pageHideHome(){
+function pageGoCur(){
     var x = document.getElementById("home");
     x.style.display = "none";
     var y = document.getElementById("cur");
     y.style.display = "flex";
+    y.style['flex-direction'] = "column";
 }
 
+function pageGoHome(){
+    var x = document.getElementById("cur");
+    x.style.display = "none";
+    var y = document.getElementById("home");
+    y.style.display = "flex";
+    y.style['flex-direction'] = "column";
+}
